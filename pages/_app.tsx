8f89b892f7333d143ec50f56d9294d27bab6ce5c
Manu2015 <devries.webdesigner@gmail.com>
2023-01-22 11:26:16 +0100

    **init** Start play with final-form
    and his React wrapper react-final-form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import '@/styles/form-form-from-rff-europe.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
